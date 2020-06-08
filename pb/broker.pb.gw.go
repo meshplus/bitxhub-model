0ad97640a22d1dc89155e6837955dd18217d56c0
zhourong <rong.zhou@outlook.com>
2020-06-08 14:19:58 +0800

    refactor(*): remove interchain_index and interchain_tx_root from BlockHeader and add interchainMeta.proto

--- pb/broker.pb.gw.go
+++ pb/broker.pb.gw.go
@@ -281,7 +281,10 @@ func local_request_ChainBroker_GetBlock_0(ctx context.Context, marshaler runtime
 	var protoReq GetBlockRequest
 	var metadata runtime.ServerMetadata
 
-	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_ChainBroker_GetBlock_0); err != nil {
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_ChainBroker_GetBlock_0); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
@@ -314,7 +317,10 @@ func local_request_ChainBroker_GetBlocks_0(ctx context.Context, marshaler runtim
 	var protoReq GetBlocksRequest
 	var metadata runtime.ServerMetadata
 
-	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_ChainBroker_GetBlocks_0); err != nil {
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_ChainBroker_GetBlocks_0); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
@@ -347,7 +353,10 @@ func local_request_ChainBroker_GetChainMeta_0(ctx context.Context, marshaler run
 	var protoReq Request
 	var metadata runtime.ServerMetadata
 
-	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_ChainBroker_GetChainMeta_0); err != nil {
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_ChainBroker_GetChainMeta_0); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
@@ -380,7 +389,10 @@ func local_request_ChainBroker_GetInfo_0(ctx context.Context, marshaler runtime.
 	var protoReq Request
 	var metadata runtime.ServerMetadata
 
-	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_ChainBroker_GetInfo_0); err != nil {
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_ChainBroker_GetInfo_0); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
