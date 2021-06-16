170f9f80d3bd1ba73ac8344ead030e90965ff756
DiyiWi <809290733@qq.com>
2021-06-16 19:10:49 +0800

    feat(*):advice lock event struct

--- pb/interchain_meta.pb.go
+++ pb/interchain_meta.pb.go
@@ -451,10 +451,7 @@ func (m *InterchainMetaS) Unmarshal(dAtA []byte) error {
 			if err != nil {
 				return err
 			}
-			if skippy < 0 {
-				return ErrInvalidLengthInterchainMeta
-			}
-			if (iNdEx + skippy) < 0 {
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
 				return ErrInvalidLengthInterchainMeta
 			}
 			if (iNdEx + skippy) > l {
@@ -644,10 +641,7 @@ func (m *InterchainS) Unmarshal(dAtA []byte) error {
 			if err != nil {
 				return err
 			}
-			if skippy < 0 {
-				return ErrInvalidLengthInterchainMeta
-			}
-			if (iNdEx + skippy) < 0 {
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
 				return ErrInvalidLengthInterchainMeta
 			}
 			if (iNdEx + skippy) > l {
