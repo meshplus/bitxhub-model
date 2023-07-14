d6ed4189c133313fff2521690781ab908dffeae8
parsleye <parsleye@163.com>
2023-07-14 17:53:50 +0800

    add ha message type

--- pb/block.pb.go
+++ pb/block.pb.go
@@ -22,7 +22,7 @@ var _ = math.Inf
 // is compatible with the proto package it is being compiled against.
 // A compilation error at this line likely means your copy of the
 // proto package needs to be updated.
-const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
+const _ = proto.GoGoProtoPackageIsVersion2 // please upgrade the proto package
 
 type Block struct {
 	BlockHeader  *BlockHeader                                `protobuf:"bytes,1,opt,name=block_header,json=blockHeader,proto3" json:"block_header,omitempty"`
@@ -933,7 +933,6 @@ func (m *BlockHeader) Unmarshal(dAtA []byte) error {
 func skipBlock(dAtA []byte) (n int, err error) {
 	l := len(dAtA)
 	iNdEx := 0
-	depth := 0
 	for iNdEx < l {
 		var wire uint64
 		for shift := uint(0); ; shift += 7 {
@@ -965,8 +964,10 @@ func skipBlock(dAtA []byte) (n int, err error) {
 					break
 				}
 			}
+			return iNdEx, nil
 		case 1:
 			iNdEx += 8
+			return iNdEx, nil
 		case 2:
 			var length int
 			for shift := uint(0); ; shift += 7 {
@@ -987,30 +988,55 @@ func skipBlock(dAtA []byte) (n int, err error) {
 				return 0, ErrInvalidLengthBlock
 			}
 			iNdEx += length
-		case 3:
-			depth++
-		case 4:
-			if depth == 0 {
-				return 0, ErrUnexpectedEndOfGroupBlock
+			if iNdEx < 0 {
+				return 0, ErrInvalidLengthBlock
 			}
-			depth--
+			return iNdEx, nil
+		case 3:
+			for {
+				var innerWire uint64
+				var start int = iNdEx
+				for shift := uint(0); ; shift += 7 {
+					if shift >= 64 {
+						return 0, ErrIntOverflowBlock
+					}
+					if iNdEx >= l {
+						return 0, io.ErrUnexpectedEOF
+					}
+					b := dAtA[iNdEx]
+					iNdEx++
+					innerWire |= (uint64(b) & 0x7F) << shift
+					if b < 0x80 {
+						break
+					}
+				}
+				innerWireType := int(innerWire & 0x7)
+				if innerWireType == 4 {
+					break
+				}
+				next, err := skipBlock(dAtA[start:])
+				if err != nil {
+					return 0, err
+				}
+				iNdEx = start + next
+				if iNdEx < 0 {
+					return 0, ErrInvalidLengthBlock
+				}
+			}
+			return iNdEx, nil
+		case 4:
+			return iNdEx, nil
 		case 5:
 			iNdEx += 4
+			return iNdEx, nil
 		default:
 			return 0, fmt.Errorf("proto: illegal wireType %d", wireType)
 		}
-		if iNdEx < 0 {
-			return 0, ErrInvalidLengthBlock
-		}
-		if depth == 0 {
-			return iNdEx, nil
-		}
 	}
-	return 0, io.ErrUnexpectedEOF
+	panic("unreachable")
 }
 
 var (
-	ErrInvalidLengthBlock        = fmt.Errorf("proto: negative length found during unmarshaling")
-	ErrIntOverflowBlock          = fmt.Errorf("proto: integer overflow")
-	ErrUnexpectedEndOfGroupBlock = fmt.Errorf("proto: unexpected end of group")
+	ErrInvalidLengthBlock = fmt.Errorf("proto: negative length found during unmarshaling")
+	ErrIntOverflowBlock   = fmt.Errorf("proto: integer overflow")
 )
